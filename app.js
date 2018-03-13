fbccf2fbd75e38e4b9484af2927553ca46ed905c
Dig <dig.vargas@locusenergy.com>
2018-03-13 12:49:00 -0700

    init routes

---
+++ app.js
@@ -0,0 +1,50 @@
+var express = require("express");
+var app = express();
+// app.use(express.logger());
+
+// var mysql = require('mysql');
+var mysql = require('./dbcon.js');
+var handlebars = require('express-handlebars').create({defaultLayout:'main', extname: '.hbs'});
+
+app.engine('hbs', handlebars.engine);
+app.set('view engine', 'hbs');
+
+var port = process.env.PORT || 3000;
+app.listen(port);
+
+var createString = "CREATE TABLE workouts("+
+    "id INT PRIMARY KEY AUTO_INCREMENT,"+
+    "name VARCHAR(255) NOT NULL,"+
+    "reps INT,"+
+    "weight INT,"+
+    "date DATE,"+
+    "lbs BOOLEAN)";
+
+app.get('/',function(req,res){
+  res.render('home');
+});
+
+app.get('/reset-table',function(req,res,next){
+  var context = {};
+  mysql.pool.query("DROP TABLE IF EXISTS workouts", function(err){ //replace your connection pool with the your variable containing the connection pool
+    mysql.pool.query(createString, function(err){
+      context.results = "Table reset";
+      console.log(context);
+      res.render('home',context);
+    })
+  });
+});
+
+app.get('/insert', function(req,res,next) {
+  console.log(req);
+  var context = {};
+  mysql.pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.c], function(err, result){
+    if(err){
+      next(err);
+      return;
+    }
+    context.results = "Inserted id " + result.insertId;
+    console.log(context);
+    res.render('home',context);
+  });
+});
--- dbcon.js
+++ dbcon.js
@@ -1,13 +1,13 @@
-const mysql = require('mysql');
+var mysql = require('mysql');
 
-const osu = {
+var osu = {
 	connectionLimit : 10,
 	host            : 'classmysql.engr.oregonstate.edu',
 	user            : 'cs290_vargassa', 	// replace YOURONID with your ONID u/n (what comes before @oregonstate.edu for your email/login credentials)
-	password        : '6453',	// replace with your password -- if left as the default, it should be the last 4 digits of your student ID
+	password        : '6449',	// replace with your password -- if left as the default, it should be the last 4 digits of your student ID
 	database        : 'cs290_vargassa'	// see comment for 'user' key above
 };
 
-let pool = mysql.createPool(osu);
+var pool = mysql.createPool(osu);
 
 module.exports.pool = pool;
